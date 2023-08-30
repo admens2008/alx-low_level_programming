00519dfabb7a859763e1327e30e0c2544df8cf70
Emmanuel Adjei-Mensah <adjeimensah@gmail.com>
2023-08-30 01:19:27 -0700

    Task 4-6 corrections

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,7 +3,7 @@
 int actual_sqrt_recursion(int n, int i);
 
 /**
- * _sqrt_recursion - a function that returns the natural square root of a number
+ * _sqrt_recursion - that returns the natural square root of a number
  * @n: number to calculate the square root of
  *
  * Return: the resulting square root
